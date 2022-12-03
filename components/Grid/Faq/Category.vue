e93e1aea3a649cf2027084c3aec71ea88014b90a
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-12-03 20:09:58 +0100

    Added faq

---
+++ components/Grid/Faq/Category.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-wrap items-start justify-center p-6">
+    <a
+      class="relative px-3 py-2 m-1 text-sm rounded-md shadow-sm sm:py-2 sm:text-base ring ring-transparent group md:px-4 hover:ring hover:ring-opacity-50 focus:ring-opacity-50 hover:ring-blue-600 text-gray-50 bg-blue-500 no-underline"
+      href="/components/article"
+      ><span
+        class="absolute top-0 right-0 p-1 text-xs opacity-0 group-hover:opacity-100 text-gray-100"
+      >
+        4 </span
+      ><span class="text-md">Article</span></a
+    ><a
+      class="relative px-3 py-2 m-1 text-sm rounded-md shadow-sm sm:py-2 sm:text-base ring ring-transparent group md:px-4 hover:ring hover:ring-opacity-50 focus:ring-opacity-50 hover:ring-blue-600 text-gray-50 bg-blue-500 no-underline"
+      href="/components/avatar"
+      ><span
+        class="absolute top-0 right-0 p-1 text-xs opacity-0 group-hover:opacity-100 text-gray-100"
+      >
+        3 </span
+      ><span class="text-md">Avatar</span></a
+    >
+  </div>
+</template>
